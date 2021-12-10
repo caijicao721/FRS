3fa19ac488032d789c022d60742d7272b5e6f798
min76 <17621778372@163.com>
2021-12-10 19:22:49 +0800

    建立了springboot项目

---
+++ src/test/java/com/cao/frs/FrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cao.frs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
